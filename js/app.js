983e1323da71fb3fa5d80521be41fb15ff676240
Ceroniko <ikomarisiy@gmail.com>
2020-09-26 21:15:12 +0300

    init commit

---
+++ js/app.js
@@ -0,0 +1,84 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // this function runs when the DOM is ready, i.e. when the document has been parsed
+
+  // all js code should go below this line
+  const clickHandler = () => {
+    document.querySelector(".header-menu__burger").classList.toggle("active");
+    document.querySelector(".header-menu__list").classList.toggle("active");
+    document.querySelector("body").classList.toggle("lock");
+  };
+
+  const headerItemClickHandler = (event) => {
+    document.querySelector("body").classList.remove("lock");
+    document.querySelector(".header-menu__list").classList.remove("active");
+    document.querySelector(".header-menu__burger").classList.remove("active");
+
+    event.preventDefault();
+    let source = event.target.innerHTML;
+    let dest = document
+      .querySelector("." + source.toLowerCase())
+      .getBoundingClientRect().top;
+
+    window.scrollTo({
+      top: dest,
+      behavior: "smooth",
+    });
+  };
+
+  const footerItemClickHandler = (event) => {
+    event.preventDefault();
+    let source = event.target.innerHTML;
+    let dest = document
+      .querySelector("." + source.toLowerCase())
+      .getBoundingClientRect();
+    dest = dest.top + pageYOffset;
+    window.scrollTo({
+      top: dest,
+      behavior: "smooth",
+    });
+  };
+
+  const contactClickHandler = (event) => {
+    document.querySelector("body").classList.remove("lock");
+    document.querySelector(".header-menu__list").classList.remove("active");
+    document.querySelector(".header-menu__burger").classList.remove("active");
+    window.scrollTo({
+      top: 3000,
+      behavior: "smooth",
+    });
+    event.preventDefault();
+    document.forms[0].elements[0].focus();
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    let email = document.forms[0].elements[0].value;
+    let message = document.forms[0].elements[1].value;
+    let user = {
+      email,
+      message,
+    };
+    fetch("https://examle.com/message", {
+      method: "POST",
+      body: JSON.stringify(user),
+    }).then((response) => console.log(response.json()));
+  };
+
+  document
+    .querySelector(".header-menu__burger")
+    .addEventListener("click", clickHandler);
+
+  for (let node of document.querySelectorAll(".header-menu__item")) {
+    node.addEventListener("click", headerItemClickHandler);
+  }
+
+  document
+    .querySelector(".header-menu__contact")
+    .addEventListener("click", contactClickHandler);
+
+  for (let node of document.querySelectorAll(".footer-menu__item")) {
+    node.addEventListener("click", footerItemClickHandler);
+  }
+
+  document.forms[0].addEventListener("submit", submitHandler);
+});
